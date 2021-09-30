75b641621bb5107b63e5e91430d3d74ea6de927f
[Erik Bahena] <[erikjbahena@gmail.com]>
2021-09-29 19:41:09 -0700

    added a create a new article feature

--- components/Article.js
+++ components/Article.js
@@ -1,7 +1,7 @@
 // This is the data we will be using to create our articles. Look at it, then proceed to line 93.
 // OPTIONAL: if you're feeling adventurous, try to make this data an export from a different module, and import it here.
 // You can read about ES6 modules here: https://exploringjs.com/es6/ch_modules.html#sec_basics-of-es6-modules
-const data = [
+export const data = [
   {
     title: 'Lambda School Students: "We\'re the best!"',
     date: "Nov 5th, 2018",
@@ -93,33 +93,33 @@ const data = [
     firstParagraph: `Text TextText Text Text TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText TextText Text`,
     secondParagraph: `Text Text Text Text Text Text Text Text Text vText Text Text Text Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text Text`,
     thirdParagraph: `Text Text Text Text Text Text Text Text Text vText Text Text Text Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text TextText Text Text`,
-
-  }
+  },
 ];
+
 /*
-  Step 1: Write a component called 'articleMaker' to create an article.
-  Your component is a function that takes an article object as its only argument,
-  and returns a DOM node looking like the one below:
+Step 1: Write a component called 'articleMaker' to create an article.
+Your component is a function that takes an article object as its only argument,
+and returns a DOM node looking like the one below:
 
-  <div class="article">
-    <h2>{title of the article}</h2>
-    <p class="date">{date of the article}</p>
+<div class="article">
+<h2>{title of the article}</h2>
+<p class="date">{date of the article}</p>
 
-    {three separate paragraph elements}
+{three separate paragraph elements}
 
-    <span class="expandButton">+</span>
-  </div>
+<span class="expandButton">+</span>
+</div>
 
-  Step 2: Still inside `articleMaker`, add an event listener to the span.expandButton.
-  This listener should toggle the class 'article-open' on div.article.
+Step 2: Still inside `articleMaker`, add an event listener to the span.expandButton.
+This listener should toggle the class 'article-open' on div.article.
 
-  Step 3: Don't forget to return something from your function!
+Step 3: Don't forget to return something from your function!
 
-  Step 4: Outside your function now, loop over the data. At each iteration you'll use your component
-  to create a div.article element and append it to the DOM inside div.articles (see index.html).
+Step 4: Outside your function now, loop over the data. At each iteration you'll use your component
+to create a div.article element and append it to the DOM inside div.articles (see index.html).
 
-  Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
-  Refresh the page to see the new article.
+Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
+Refresh the page to see the new article.
 */
 function articleMaker({
   title,
@@ -127,17 +127,18 @@ function articleMaker({
   firstParagraph,
   secondParagraph,
   thirdParagraph,
+  _,
 }) {
   const html = `
   <div class="article">
   <h2>${title}</h2>
   <p class="date">${date}</p>
-
+  
   <p>${firstParagraph}</p>
   <p>${secondParagraph}</p>
-
+  
   <p>${thirdParagraph}</p>
-
+  
   <span class="expandButton">+</span>
   </div>
   `;
@@ -154,9 +155,18 @@ function articleMaker({
   return articleEl;
 }
 
-data.forEach((articleData) => {
-  const articleEl = articleMaker(articleData);
-  const articlesContainer = document.querySelector(".articles");
 
-  articlesContainer.append(articleEl);
-});
+
+export const updateArticles = function (articlesArr) {
+  const articlesContainer = document.querySelector(".articles");
+  articlesContainer.innerHTML = ""
+
+  articlesArr.forEach((articleObj) => {
+    const articleEl = articleMaker(articleObj);
+    articlesContainer.append(articleEl);
+  });
+};
+
+updateArticles(data)
+
+export default [updateArticles, data];
